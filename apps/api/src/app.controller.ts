28b609fa3a339eb6e3a577062db40a0589fd93e2
Steven Yung <stevenyung@fastmail.com>
2021-11-10 14:30:29 +0100

    implement basic AppController@index for listing cities by category

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { City } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  index(): Promise<number> {
+  async index(): Promise<{ metropolis: City[]; overseas: City[] }> {
+    const cities = await this.prismaService.city.findMany({
+      take: 100,
+      orderBy: {
+        municipalityName: 'asc',
+      },
+    });
+
+    const metropolis = [];
+    const overseas = [];
+
+    for (let i = 0; i < cities.length; i++) {
+      const city = cities[i];
+
+      parseInt(city.zipCode) < 97000
+        ? metropolis.push(city)
+        : overseas.push(city);
+    }
+
+    return { metropolis, overseas };
+  }
+
+  @Get('/count')
+  count(): Promise<number> {
     return this.prismaService.city.count();
   }
 }
